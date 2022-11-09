51ec59b18a76a3fdd44eab313b28d2fb1a74d043
kahshiu-ipp <114044179+kahshiu-ipp@users.noreply.github.com>
2022-11-10 07:25:41 +0800

    added sql backup

---
+++ demand_insight_202211100722.sql
@@ -0,0 +1,40 @@
+INSERT INTO public.demand_insight (proj_name,proj_type,proj_rooms,facilities,unit_avail,demand,demand_score,radii) VALUES
+	 ('Aster Green','Villa',2,'Swimming Pool',34,809,4.20,10),
+	 ('Skyluxe on the Park','Condominium',3,'Swimming Pool',6,34,17.65,4),
+	 ('M Oscar','Semi Detached',2,'',91,113,80.53,7),
+	 ('Bloomsvale','Semi Detached',2,'',7,531,1.32,11),
+	 ('The Tropika','Semi Detached',2,'',105,157,66.88,4),
+	 ('Twin Arkz','Semi Detached',2,'',10,914,1.09,10),
+	 ('Aster Green','Bungalow',2,'',28,988,2.83,3),
+	 ('JRK Convena','Condominium',2,'',30,642,4.67,8),
+	 ('The Park 2','Condominium',2,'',2,756,0.26,14),
+	 ('Project 4','Villa',3,'Gym',21,887,2.37,13);
+INSERT INTO public.demand_insight (proj_name,proj_type,proj_rooms,facilities,unit_avail,demand,demand_score,radii) VALUES
+	 ('Project 6','Condominium',3,'Gym',43,183,23.50,12),
+	 ('Project 12','Condominium',3,'Tennis Court',5,637,0.78,5),
+	 ('Project 13','Condominium',3,'Gym',7,677,1.03,13),
+	 ('Project 14','Condominium',4,'BBQ',8,418,1.91,9),
+	 ('Project 2','Terrace',3,'Tennis Court',21,65,32.31,9),
+	 ('Project 18','Terrace',2,'Swimming Pool',9,680,1.32,6),
+	 ('Project 1','Semi Detached',3,'',25,233,10.73,6),
+	 ('Project 5','Semi Detached',3,'',71,88,80.68,14),
+	 ('Project 9','Semi Detached',3,'',53,417,12.71,2),
+	 ('Project 10','Semi Detached',2,'',2,24,8.33,2);
+INSERT INTO public.demand_insight (proj_name,proj_type,proj_rooms,facilities,unit_avail,demand,demand_score,radii) VALUES
+	 ('Project 11','Semi Detached',3,'',425,845,50.30,5),
+	 ('Project 19','Semi Detached',3,'',3,688,0.44,14),
+	 ('Maple Residence','Terrace',3,'',13,714,1.82,3),
+	 ('Maple Residence (Tower B & C)','Terrace',3,'',9,164,5.49,12),
+	 ('Rosewoodz','Terrace',2,'',4,695,0.58,5),
+	 ('Aster Green','Terrace',2,'',1,167,0.60,10),
+	 ('Bintang','Terrace',2,'',52,541,9.61,1),
+	 ('Citizen 2','Terrace',2,'',5,89,5.62,9),
+	 ('The Link 2','Terrace',3,'',8,928,0.86,7),
+	 ('The Link 2','Terrace',2,'',6,514,1.17,6);
+INSERT INTO public.demand_insight (proj_name,proj_type,proj_rooms,facilities,unit_avail,demand,demand_score,radii) VALUES
+	 ('Project 3','Terrace',3,'',5,671,0.75,2),
+	 ('Project 15','Terrace',3,'',10,315,3.17,2),
+	 ('Project 16','Terrace',4,'',3,224,1.34,8),
+	 ('Project 17','Terrace',3,'',10,407,2.46,10),
+	 ('Project 7','Bungalow',3,'',10,256,3.91,2),
+	 ('Project 8','Bungalow',3,'',142,351,40.46,8);
